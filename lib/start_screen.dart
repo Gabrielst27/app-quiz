318e6d2a7e48ac74a17fbe47968359da34d4250c
Gabriel Torres <gabrielst328@gmail.com>
2025-05-27 17:32:47 -0300

    refac: added opacity to start_screen image and icon to start button

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -11,17 +11,22 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset(logoImage, width: 300),
+          Image.asset(
+            logoImage,
+            width: 300,
+            color: const Color.fromARGB(127, 255, 255, 255),
+          ),
           const SizedBox(height: 80),
           const Text(
             'Teste seus conhecimentos!',
             style: TextStyle(color: Colors.white, fontSize: 24),
           ),
           const SizedBox(height: 80),
-          OutlinedButton(
+          OutlinedButton.icon(
             onPressed: () {},
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
-            child: Text('Iniciar'),
+            icon: Icon(Icons.start_sharp),
+            label: Text('Iniciar'),
           ),
         ],
       ),
